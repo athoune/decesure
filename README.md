bc5b4508d3d581a10b8cf62051aff9a14857cda6
Mathieu Lecarme <mathieu@garambrogne.net>
2023-03-27 18:19:56 +0200

    read wikipedia abstract dump

---
+++ README.md
@@ -0,0 +1,4 @@
+# Décésure
+
+Go get some abstract.xml.gz file somewhere in http://ftp.acc.umu.se/mirror/wikimedia.org/dumps/enwiki/
+
---
+++ decesure.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import io
+from collections import Counter
+from xml import sax
+from xml.sax.handler import ContentHandler
+import pickle
+
+MYTAGS = ["title", "abstract"]
+
+
+class AbstractWikiHandler(ContentHandler):
+    buffer = io.StringIO()
+    path = []
+    counter = Counter()
+
+    def startElement(self, name, attrs):
+        self.path.append(name)
+        if name in MYTAGS:
+            self.buffer = io.StringIO()
+
+    def characters(self, content):
+        if self.path[-1] in MYTAGS:
+            self.buffer.write(content)
+
+    def endElement(self, name):
+        self.path.pop()
+        if name in MYTAGS:
+            value = self.buffer.getvalue()
+            if name == "title":
+                value = value[11:]
+            self.counter.update(w for w in value.lower().split() if len(w) > 1)
+
+    def dump(self, f):
+        "dump counter to pickle format"
+        pickle.dump(dict(self.counter), f)
+
+
+if __name__ == "__main__":
+    import sys
+    import gzip
+
+    h = AbstractWikiHandler()
+    with gzip.open(sys.argv[1], "r") as f:
+        sax.parse(f, h)
+    print(h.counter.most_common(128))
+    print(h.counter.total())
+    h.dump(open('en.pickle', 'wb'))
